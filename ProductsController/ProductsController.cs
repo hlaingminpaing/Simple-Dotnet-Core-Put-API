2ed0aefc1b2d14bc46496e9e9dd7c12c8799a6cd
hlaingminpaing <hlaingminpaing.ygn@gmail.com>
2024-05-17 13:34:14 +0630

    first commit

---
+++ ProductsController/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplePutApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimplePutApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product1", Price = 10.0m },
+            new Product { Id = 2, Name = "Product2", Price = 20.0m }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product updatedProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+
+            return NoContent();
+        }
+    }
+}
